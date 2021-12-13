27bb66d80bfb1fdbf4d31873c09b32707cae79b9
Cyytrus <antoniopaulo122@gmail.com>
2021-12-13 15:19:39 -0300

    add a Simple Menu example in C

--- Menus.c
+++ Menus.c
@@ -1,10 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Exemplo de Menu utilizando while e switch case
+*/
+
 int main()
 {
 
-  
+  int opcao;
+
+  while(opcao < 1 || opcao > 5)
+  {
+
+    printf("Escolha uma opcao:\n");
+    printf("1 - Opcao 1\n");
+    printf("2 - Opcao 2\n");
+    printf("3 - Opcao 3\n");
+    printf("4 - Opcao 4\n");
+    printf("5 - Opcao 5\n");
+
+    scanf("%d", &opcao);
+
+    switch(opcao)
+    {
+      case 1:
+        printf("Opcao 1 foi escolhida\n");
+        break;
+      case 2:
+        printf("Opcao 2 foi escolhida\n");
+        break;
+      case 3:
+        printf("Opcao 3 foi escolhida\n");
+        break;
+      case 4:
+        printf("Opcao 4 foi escolhida\n");
+        break;
+      case 5:
+        printf("Opcao 5 foi escolhida\n");
+        break;
+      default:
+      printf("Opcao invalida\n");
+    }
+  }
 
   return 0;
 }
